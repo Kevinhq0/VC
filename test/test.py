68f6909e5fa81201c01a6b1ad5ccfa74ddcf32e1
Kevinhq0 <90949106+Kevinhq0@users.noreply.github.com>
2022-09-06 14:57:31 -0400

    Add files via upload
    Changed the line to add the load_json function from the add.py file.

--- test/test.py
+++ test/test.py
@@ -1,10 +1,18 @@
 
 import sys,os
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-from scripts.add import load_json
+
+def load_json():
+    with open('../Downloads/response.json') as json_obj:
+        response = json.load(json_obj)
+    return response
 
 
 def test_load_json():
   response = load_json()
   assert isinstance(response, dict)
   assert response.get("Data Glacier") =="Cricket"
+
+
+
+
